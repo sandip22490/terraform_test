7ca1fe674013f52e90ef309667bfa7cad88f3e53
sandip22490 <sandip22490@gmail.com>
2018-03-19 23:34:10 +0530

    Remove project name variable.

--- variable.tf
+++ variable.tf
@@ -1,7 +1,10 @@
 variable "access_key" {}
 variable "secret_key" {}
 variable "stage" {}
-variable "project_name" {}
+
+variable "project_name" {
+  default = "infra"
+}
 
 variable "region" {
   default = "us-east-1"

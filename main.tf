cc2a4c0cbcee7b28772e575c00dfaa4d653aa11a
sandip22490 <sandip22490@gmail.com>
2018-03-24 13:50:02 +0530

    Updated buildspec to work with terragrunt

--- main.tf
+++ main.tf
@@ -1,3 +1,5 @@
+#specific version
+
 # Initializing provider
 
 provider "aws" {
@@ -12,46 +14,9 @@ data "aws_caller_identity" "current" {}
 
 terraform {
   # The configuration for this backend will be filled in by Terragrunt
-  backend "s3" {}
+  backend          "s3"             {}
+  required_version = "0.11.5"
 }
-
-# create S3 bucket to store the terraform remote state
-
-# resource "aws_s3_bucket" "terraform_remote_state" {
-#   bucket = "${var.project_name}-terraform-remote-state"
-
-#   versioning {
-#     enabled = true
-#   }
-# }
-
-# Create dynamodb table for locking the remote state file
-
-# resource "aws_dynamodb_table" "terraform_remote_state" {
-#   name           = "${var.project_name}-terraform-remote-state"
-#   hash_key       = "LockID"
-#   read_capacity  = 1
-#   write_capacity = 1
-
-#   attribute {
-#     name = "LockID"
-#     type = "S"
-#   }
-# }
-
-# Create terraform remote state backend
-# When initializing stage for the first time, disable this and then enable it for remote state.
-
-# terraform {
-#   backend "s3" {
-#     encrypt        = true
-#     bucket         = "maxxsure-terraform-remote-state"
-#     dynamodb_table = "maxxsure-terraform-remote-state"
-#     region         = "us-east-1"
-#     key            = "terraform.tfstat"
-#     profile        = "maxxsure-dev"
-#   }
-# }
 
 resource "aws_dynamodb_table" "temp-table" {
   name           = "${var.project_name}-temp-table"
